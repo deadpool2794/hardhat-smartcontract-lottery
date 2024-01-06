74f8d9565469de109ae234ee1e1057c1bcd45eaf
itaachi_uchiha <abdulalimkhan10@gmail.com>
2024-01-05 21:27:53 -0500

    initial commit

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -14,7 +14,7 @@ error Raffle__SendMoreToEnterRaffle();
 error Raffle__RaffleNotOpen();
 
 /**@title A sample Raffle Contract
- * @author Alim Khan Abdul
+ * @author Abdul Alim
  * @notice This contract is for creating a sample raffle contract
  * @dev This implements the Chainlink VRF Version 2
  */
